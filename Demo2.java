60b7a50f5b2e5accd0baf92f250b95146b961764
akankshaeng <akankshasen188@gmail.com>
2026-05-27 08:55:05 +0530

    commited

---
+++ Demo2.java
@@ -0,0 +1,27 @@
+public class Demo2 {
+    public static void main(String[] args){
+        //Integers --> byte, short, int, long
+        byte b = 5;
+        short s = 20;
+        int i = 2000;
+        long l = 30000;
+
+        // Real numbers
+        float f = 10.54f;
+        double d = 23.0987;
+
+        // Characters 
+        char c = 'a'; // 'a' --> integer --> binary --> store
+
+        //boolean
+        boolean bool = false;
+
+        System.out.println("Integer values --> " + b +  " , " + s +  " , "  + i +  " , "  + l);
+        System.out.println("Floating values --> "  + f +  " , "  + d);
+        System.out.println("Character values --> " + c);
+        System.out.println("boolean values --> " + bool);
+
+        
+
+    }
+}
