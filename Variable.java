859c000859f2218a6fc04d7fcb5902b1cab2b145
akankshaeng <akankshasen188@gmail.com>
2026-07-10 01:37:48 +0530

    commited

---
+++ Variable.java
@@ -0,0 +1,29 @@
+public class Variable{
+       public static void main(String[] args) {
+        
+          // Valid variable names
+          int age = 20;
+          String studentName = "Akanksha";
+          double salary_Amount = 50000.60;
+          int $count = 10;
+          int _value = 100;
+
+          System.out.println("Age: " + age);
+          System.out.println("Student Name: " + studentName);
+          System.out.println("Salary: " + salary_Amount);
+          System.out.println("Count: " + $count);
+          System.out.println("Value: " + _value);
+
+          // Invalid variable names (Uncommenting these will cause errors)
+
+         // int 1number = 10;         //Cannot start with a digit
+         // int student-name = 29;   //Hyphen (-) not alllowed
+         // int my value = 30;       //Spaces are not allowed
+         // int class = 40;         //Reserved keyword cannot be used 
+
+
+
+
+
+     }
+}
