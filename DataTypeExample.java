ede57c2db7e8ed9ca061383bedb068cfdc158411
akankshaeng <akankshasen188@gmail.com>
2026-07-24 04:08:30 +0530

    commited

---
+++ DataTypeExample.java
@@ -0,0 +1,22 @@
+public class DataTypeExample {
+    public static void main(String[] args) {
+
+        byte age = 20;
+        int marks = 95;
+        long population = 1400000000L;
+        float percentage = 92.5f;
+        double salary = 45000.75;
+        char grade = 'A';
+        boolean pass = true;
+        String name = "Akanksha";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Population: " + population);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Pass: " + pass);
+    }
+}
